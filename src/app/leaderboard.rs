4feb3fddbdd61d81b319e455c817ddb0db443c08
Kait Wang <sibevin@gmail.com>
2024-01-06 21:18:04 +0800

    Fix format

--- src/app/leaderboard.rs
+++ src/app/leaderboard.rs
@@ -43,42 +43,18 @@ impl LeaderboardRecord {
 
     pub fn fetch(&self, field: &str) -> u32 {
         match field {
-            "time" => {
-                self.time
-            }
-            "score" => {
-                self.score
-            }
-            "max_alpha_count" => {
-                self.max_alpha_count
-            }
-            "max_control_chain" => {
-                self.max_control_chain
-            }
-            "max_hyper_chain" => {
-                self.max_hyper_chain
-            }
-            "total_control_count" => {
-                self.total_control_count
-            }
-            "total_hyper_count" => {
-                self.total_hyper_count
-            }
-            "max_control_count" => {
-                self.max_control_count
-            }
-            "max_full_level_control_count" => {
-                self.max_full_level_control_count
-            }
-            "max_control_level" => {
-                self.max_control_level
-            }
-            "max_hyper_level" => {
-                self.max_hyper_level
-            }
-            "max_stopping_time" => {
-                self.max_stopping_time
-            }
+            "time" => self.time,
+            "score" => self.score,
+            "max_alpha_count" => self.max_alpha_count,
+            "max_control_chain" => self.max_control_chain,
+            "max_hyper_chain" => self.max_hyper_chain,
+            "total_control_count" => self.total_control_count,
+            "total_hyper_count" => self.total_hyper_count,
+            "max_control_count" => self.max_control_count,
+            "max_full_level_control_count" => self.max_full_level_control_count,
+            "max_control_level" => self.max_control_level,
+            "max_hyper_level" => self.max_hyper_level,
+            "max_stopping_time" => self.max_stopping_time,
             _ => panic!("Invalid field"),
         }
     }
